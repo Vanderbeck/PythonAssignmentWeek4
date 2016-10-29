ddff3074659ca17a2f61942a58ffa76520e7f127
R Lindsay Vanderbeck <9rlv@queensu.ca>
2016-10-29 12:55:35 -0600

    R.E. and variabe types now work. Correct sum verified.

--- A1.py
+++ A1.py
@@ -1,15 +1,27 @@
+#Import Libraries
 import urllib
-import re
 from BeautifulSoup import *
+#Set regular expressions
+import re
+R = re.compile('[0-9]+')
 
+#Open and interpret a URL
 html = urllib.urlopen('http://python-data.dr-chuck.net/comments_42.html').read()
 soup = BeautifulSoup(html)
 
-ClassComments = soup('span')
-R = re.compile('comments.+')
+#Querry BeautifulSoup. return 'span' variables
+ClassComments =soup('span')
 
-for line in ClassComments:
-    line = str(line)
-    nums = R.findall(line)
+#Initialize loop variables
+total = int(0)
+CC_str = str(ClassComments)
 
-print nums
+#Seperate numbers from the lines and add them
+nums_list = R.findall(CC_str)
+
+for nums in nums_list:
+    nums = int(nums)
+    total = total + nums
+
+#Print Result
+print total
