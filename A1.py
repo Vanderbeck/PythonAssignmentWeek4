f490a20c1afb6661d2c40ea31613c1e39a7dcea8
R Lindsay Vanderbeck <9rlv@queensu.ca>
2016-10-29 11:59:02 -0600

    The regular expression syntax is working.

--- A1.py
+++ A1.py
@@ -1,7 +1,15 @@
 import urllib
+import re
 from BeautifulSoup import *
 
 html = urllib.urlopen('http://python-data.dr-chuck.net/comments_42.html').read()
+soup = BeautifulSoup(html)
 
+ClassComments = soup('span')
+R = re.compile('comments.+')
 
-print html
+for line in ClassComments:
+    line = str(line)
+    nums = R.findall(line)
+
+print nums
